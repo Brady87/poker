8b1fd403e9e165ff09015238d687c4b094fe83f3
Thomas Brard <thomasbrard87@gmail.com>
2020-05-06 14:39:50 +0200

    Second push

--- Poker/main.cpp
+++ Poker/main.cpp
@@ -5,14 +5,6 @@ using namespace std;
 int main() {
 	//Affichage de "Hello World"
 	cout << "Hello World !"<<endl;
-	double p = 1 / 512;
-	double s = 0;
-	int nbre = 0;
-	cout << "Saisir le nombre de répétition" << endl;
-	cin >> nbre;
-	for (int i = 0; i < nbre; i++) {
-		s = pow((511 / 512), i);
-	}
-	cout << "Probabilite chromatique apres " << nbre << " repetitions : " << s << "." << endl;
+	int i = 0;
 	return 0;
 }
